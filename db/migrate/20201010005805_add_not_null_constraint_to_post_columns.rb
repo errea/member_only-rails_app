501fdd66dc7300c403c1dec98b0837f22db95bc2
errea <erreakezy@gmail.com>
2021-04-21 21:53:27 +0100

    migrations for constraint

---
+++ db/migrate/20201010005805_add_not_null_constraint_to_post_columns.rb
@@ -0,0 +1,6 @@
+class AddNotNullConstraintToPostColumns < ActiveRecord::Migration[6.0]
+  def change
+    change_column_null :posts, :title, false
+    change_column_null :posts, :body, false
+  end
+end
