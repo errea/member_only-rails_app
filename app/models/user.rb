1baf4852a1c8d92ff4dcb68fb5ab942cbd006a07
errea <erreakezy@gmail.com>
2021-04-22 13:33:17 +0100

    Rubocop fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,21 +13,21 @@
 #  username               :string           not null
 #
 class User < ApplicationRecord
-       # Include default devise modules. Others available are:
-       # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-       devise :database_authenticatable, :registerable,
-       :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
-       validates :username, uniqueness: true
+  validates :username, uniqueness: true
 
-       has_many :posts,
-       foreign_key: :author_id,
-       dependent: :destroy
+  has_many :posts,
+           foreign_key: :author_id,
+           dependent: :destroy
 
-       has_many :likes,
-       dependent: :destroy
+  has_many :likes,
+           dependent: :destroy
 
-       has_many :liked_posts,
-       through: :likes,
-       source: :post
+  has_many :liked_posts,
+           through: :likes,
+           source: :post
 end
--- db/migrate/20201010005805_add_not_null_constraint_to_post_columns.rb
+++
@@ -1,6 +0,0 @@
-class AddNotNullConstraintToPostColumns < ActiveRecord::Migration[6.0]
-  def change
-    change_column_null :posts, :title, false
-    change_column_null :posts, :body, false
-  end
-end
