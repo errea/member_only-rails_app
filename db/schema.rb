847f8e11c45e4d3eb1525a0d4bdb099919d32a55
errea <erreakezy@gmail.com>
2021-04-22 13:52:01 +0100

    Modify errors in migrations db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_101040) do
+ActiveRecord::Schema.define(version: 2021_04_22_121843) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
