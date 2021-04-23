d534afd61d3ee08946198a96ea8760a07b2cb73c
errea <erreakezy@gmail.com>
2021-04-23 10:08:03 +0100

    Modify resources :posts, only: [:new, :create, :index]

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :likes
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
   
   root to: 'posts#index'
 
-  resources :posts #only: [:new, :create, :index]
+  resources :posts  only: [:new, :create, :index]
   devise_for :users
   resources :likes, only: [:create, :destroy]
   get 'about', to: 'application#about'
